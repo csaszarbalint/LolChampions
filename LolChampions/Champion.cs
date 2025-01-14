5cd000c770a27bdda8b68b61ab20277faadc3dc5
csaszarbalint <csaszar.balint@hbsz.edu.hu>
2025-01-14 11:09:27 +0100

    Add project files.

---
+++ LolChampions/Champion.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolChampions
+{
+    class Champion
+    {
+        public string Id;
+        public string Name;
+        public List<string> Classes;
+        public List<int> Numbers;
+
+        public Champion(string line)
+        {
+            string[] tokens = line.Split(';');
+
+            this.Id = tokens[0];
+            this.Name = tokens[1];
+            this.Classes = tokens[5].
+                Trim(new char[] { '[', ']' }).
+                Split(',').
+                ToList();
+            this.Numbers = tokens[6].
+                Trim(new char[] { '[', ']' }).
+                Split(',').
+                Select(e => int.Parse(e)).
+                ToList();
+        }
+    }
+}
---
+++ LolChampions/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace LolChampions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Champion> champions = new List<Champion>();
+
+            foreach(string row in File.ReadAllLines("champions.csv"))
+            {
+                champions.Add(new Champion(row));
+            }
+
+            //2.feladat
+            {
+                Console.WriteLine(champions.Count()); 
+            }
+
+            //3.feladat
+            {
+                Console.WriteLine(champions.Where(c => c.Classes.Contains("Fighter")).Count());
+            }
+
+            //4.feladat
+            {
+                Console.WriteLine(champions.Select(c => c.Numbers[3]).Average());
+            }
+
+            //5.feladat
+            {
+                Console.WriteLine(
+                    champions.Where(c => c.Classes.SingleOrDefault() == "Mage").Count() > champions.Where(c => c.Classes.SingleOrDefault() == "Support").Count() 
+                    ? "Mage" 
+                    : "Support"
+                    );
+            }
+        }
+    }
+}
